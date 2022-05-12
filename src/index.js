27e975657b3fdb7dbb59eae74369f20e16e58502
Nikola Šipláková <nikola.siplakova@student.tuke.sk>
2022-05-12 08:34:41 +0200

    comment

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      {/* <App /> */}
       <Routes>
         <Route path="/spj-app/" element={<App />} />
         <Route path="/spj-app/grammar" element={<Grammar />} />
