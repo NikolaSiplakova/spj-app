0e30215b04f984c679c7bcfc3b5812a8d3e38c03
Nikola Šipláková <nikola.siplakova@student.tuke.sk>
2022-05-12 04:17:50 +0200

    ïnitial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.scss';
+import Spj from './components/Spj/Spj';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Spj />
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
