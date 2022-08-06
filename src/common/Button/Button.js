d5f542ad0f3ee0ccc82c852940c56b5fe11e2476
Nikola Šipláková <nikola.siplakova@student.tuke.sk>
2022-08-06 15:39:23 +0200

    - Button has isActive prop now - new designs for TopBar

--- src/common/Button/Button.js
+++ src/common/Button/Button.js
@@ -5,13 +5,14 @@ import classnames from "classnames"
 import classes from "./Button.module.scss"
 
 const Button = (props) => {
-  const { icon, isExtended, label, onClick, primary, secondary } = props
+  const { icon, isActive, isExtended, label, onClick, primary, secondary } =
+    props
 
   return (
     <button
       className={classnames(classes["button"], {
         [classes["button--primary"]]: primary === true,
-        [classes["button--secondary"]]: secondary === true,
+        [classes["button--secondary"]]: secondary === true || isActive === true,
         [classes["button--collapsed"]]: isExtended === false,
       })}
       onClick={onClick}
@@ -26,15 +27,18 @@ export default Button
 
 Button.propTypes = {
   icon: PropTypes.node.isRequired,
+  isActive: PropTypes.bool,
   isExtended: PropTypes.bool.isRequired,
   label: PropTypes.string.isRequired,
-  onClick: PropTypes.func.isRequired,
+  onClick: PropTypes.func,
   primary: PropTypes.bool.isRequired, //orange bckg
   secondary: PropTypes.bool.isRequired, //white bckg
 }
 
 Button.defaultProps = {
+  isActive: false,
   isExtended: true,
+  onClick: null,
   primary: true,
   secondary: false,
 }
